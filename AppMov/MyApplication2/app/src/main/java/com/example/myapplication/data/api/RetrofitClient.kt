f8a6b64d16f73b7f1f4f9bff496b0c2718a30b7e
i-vie <123426681+i-vie@users.noreply.github.com>
2025-01-22 17:24:30 +0000

    Update RetrofitClient.kt
    Adicionado código em RetrofitClient.kt para evitar erro (explicação encontra-se no código)

--- AppMov/MyApplication2/app/src/main/java/com/example/myapplication/data/api/RetrofitClient.kt
+++ AppMov/MyApplication2/app/src/main/java/com/example/myapplication/data/api/RetrofitClient.kt
@@ -12,15 +12,30 @@ object RetrofitClient {
 
     private const val BASE_URL = "http://10.0.2.2:8000/api/"
 
-    val okHttpClient = OkHttpClient.Builder()
-        .cache(null)
-        .addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        })
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .writeTimeout(30, TimeUnit.SECONDS)
-        .build()
+    //codigo obtido através de: https://stackoverflow.com/questions/70246508/retrofit-okhttp-unexpected-end-of-stream
+    //"When Android Studio's emulators running in Windows series OS (checked for 7 & 10) receive json-typed reply 
+    //from server with retrofit it can with various probability loose 1 or 2 last symbols of the body when it is decoded to string, 
+    //this symbols contain closing curly brackets and so such body could not be parsed to object by gson converter which results in 
+    //throwing exception.
+    //The idea of workaround I found is to add an interceptor to retrofit which would check the decoded to string body if its 
+    //last symbols match those of valid json response and add them if they are missed." 
+    val stringInterceptor = Interceptor { chain: Interceptor.Chain ->
+        val request = chain.request()
+        val response = chain.proceed(request)
+        val source = response.body?.source()
+        source?.request(Long.MAX_VALUE)
+        val buffer = source?.buffer()
+        var responseString = buffer?.clone()?.readString(Charset.forName("UTF-8"))
+        if (responseString != null && responseString.isNotEmpty()) {
+            if (!responseString.trimEnd().endsWith("}")) {
+                responseString += "}"
+            }
+        }
+        val contentType = response.body?.contentType()
+        val body = ResponseBody.create(contentType, responseString ?: "")
+        return@Interceptor response.newBuilder().body(body).build()
+
+        }
 
 
     private var retrofit: Retrofit? = null
